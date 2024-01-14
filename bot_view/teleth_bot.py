480fbaa4bea9b1ad5c9314725d02b1bf6fd5a26a
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-01-14 23:32:54 +0300

    14.01

--- bot_view/teleth_bot.py
+++ bot_view/teleth_bot.py
@@ -12,7 +12,7 @@ from telethon.tl.types import ChannelParticipantsSearch
 config = configparser.ConfigParser()
 config.read("\settings\config.ini")
 
-api_id = int(config['Telegram']['api_id'])
+api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
 
