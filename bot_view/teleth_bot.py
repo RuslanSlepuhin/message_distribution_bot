f0d12847efc7c0a0745f07298e785529e3c263a1
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-01-14 23:50:00 +0300

    14.01

--- bot_view/teleth_bot.py
+++ bot_view/teleth_bot.py
@@ -12,9 +12,9 @@ from telethon.tl.types import ChannelParticipantsSearch
 config = configparser.ConfigParser()
 config.read("./settings/config.ini")
 
-api_id = config['Telegram']['api_id']
-api_hash = config['Telegram']['api_hash']
-username = config['Telegram']['username']
+api_id = config['TelegramProduction']['api_id']
+api_hash = config['TelegramProduction']['api_hash']
+username = config['TelegramProduction']['username']
 
 class TelethBot:
 
